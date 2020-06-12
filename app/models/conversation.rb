6d51ed5052af7ca47f32c57ef4890d1f1274b08d
chelsejw <chelsejw@gmail.com>
2020-06-12 16:02:00 +0800

    add token and generate token method to conversations

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,3 +1,12 @@
 class Conversation < ApplicationRecord
   belongs_to :lifeline
+
+  before_create :generate_token
+  def generate_token(length=50)
+    loop do
+      token = self.token = SecureRandom.urlsafe_base64(length, false)
+      break token unless self.class.exists?(token: token)
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_12_053203) do
+ActiveRecord::Schema.define(version: 2020_06_12_080121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_06_12_053203) do
     t.bigint "lifeline_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "token"
     t.index ["lifeline_id"], name: "index_conversations_on_lifeline_id"
   end
 
