9c4d51f7b43497a60c63d8fc51006c7e054e0696
chelsejw <chelsejw@gmail.com>
2020-06-14 00:12:40 +0800

    add email template, include actionmailer in user signup

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,2 +1,10 @@
 class UserNotifierMailer < ApplicationMailer
+      default :from => 'lifelines.team@gmail.com'
+
+  # send a signup email to the user, pass in the user object that   contains the user's email address
+  def send_signup_email(user)
+    @user = user
+    mail( :to => @user.email,
+    :subject => 'Welcome to Lifelines!' )
+  end
 end
