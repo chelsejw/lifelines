65f215cda8f81c3820a567cc8c6be41b9a2f7001
chelsejw <chelsejw@gmail.com>
2020-06-15 02:08:05 +0800

    give dashboard component access to auth state

--- app/javascript/packs/app/dashboard/Dashboard.js
+++ app/javascript/packs/app/dashboard/Dashboard.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import ProfileForm from './ProfileForm'
 import {Link, Route, useRouteMatch} from 'react-router-dom'
+import {connect} from 'react-redux'
 
 
-
-const Dashboard = ()=> {
+const Dashboard = (props)=> {
     let { path, url } = useRouteMatch();
 
     return (
       <div>
         <Link to={`${url}/profile`}>Profile</Link>
-        <Link to={`${url}/verification`}>Verification</Link>
+
+        {props.auth.currentUser.account_type !== "admin" && (
+          <Link to={`${url}/verification`}>Verification</Link>
+        )}
 
         <Route path="/dashboard/profile" exact component={ProfileForm} />
 
@@ -20,13 +23,17 @@ const Dashboard = ()=> {
           render={() => <h1>Verifications</h1>}
         />
         <Route
-        path={path}
-        exact
-        render={()=> <h1>Pick an option above to get started.</h1>}
+          path={path}
+          exact
+          render={() => <h1>Pick an option above to get started.</h1>}
         />
-                
       </div>
     );
 }
 
-export default Dashboard;
+const mapStateToProps = (state) => {
+  return {
+    auth: state.auth,
+  };
+};
+export default connect(mapStateToProps)(Dashboard);
