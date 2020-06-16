60c71880919b39ae027441fb0cd1549f25ae0fe7
chelsejw <chelsejw@gmail.com>
2020-06-16 18:59:23 +0800

    tweak api route

--- app/javascript/packs/app/constants/index.js
+++ app/javascript/packs/app/constants/index.js
@@ -1,7 +1,7 @@
 import ConversationsList from "../chat/ConversationsList";
 
 export const API_ROOT = 'http://localhost:5000';
-export const API_WS_ROOT = 'wss://localhost:3000/cable';
+export const API_WS_ROOT = 'ws://localhost:3000/cable';
 export const HEADERS = {
   'Content-Type': 'application/json',
   Accept: 'application/json',
